1e9051bca1ac523e76db4feb326d2fa0f7b9fc73
henryanandsr <13521004@std.stei.itb.ac.id>
2023-05-22 01:55:11 +0700

    fix : web scrapping

--- src/webScrapping.py
+++ src/webScrapping.py
@@ -1,40 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.kompas.com/"
-url2 = "https://www.detik.com/"
-url3 = "https://www.tribunnews.com/"
-url4 = "https://www.liputan6.com/"
-url5 = "https://www.cnbcindonesia.com/"
-url6 = "https://www.cnnindonesia.com/"
-url7 = "https://www.beritasatu.com/"
-url8 = "https://www.beritagar.id/"
-url9 = "https://www.beritajakarta.id/"
-url10 = "https://www.beritajatim.com/"
-url11 = "https://www.beritabali.com/"
-url12 = "https://www.beritajogja.id/"
-url13 = "https://www.beritamalukuonline.com/"
-url14 = "https://www.beritapapua.id/"
-url15 = "https://www.beritapapua.co/"
-url16 = "https://www.beritapapuabarat.com/"
-url17 = "https://www.beritapapuabarat.co/"
-url18 = "https://www.beritapapuabarat.id/"
-url19 = "https://www.beritapapuabarat.net/"
-url20 = "https://www.beritapapuabarat.org/"
-
-response = requests.get(url)
-
-soup = BeautifulSoup(response.text, "html.parser")
-
-judul_h1 = soup.find_all("h1")
-judul_h2 = soup.find_all("h2")
+url = "https://www.thejakartapost.com/business/economy?page="
 
 list_judul = []
-for judul in judul_h1:
-    list_judul.append(judul.text)
 
-for judul in judul_h2:
-    list_judul.append(judul.text)
+for i in range(1,50):
+    response = requests.get(url+str(i))
 
-for el in list_judul:
-    print(el)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    judul_h2 = soup.find_all("h2")
+
+    for judul in judul_h2:
+        list_judul.append(judul.text)
+
+    for el in list_judul:
+        print(el)
+with open("list_judul.txt", "w") as file:
+    for judul in list_judul:
+        cleaned_judul = judul.strip()
+        file.write(cleaned_judul + "\n")
