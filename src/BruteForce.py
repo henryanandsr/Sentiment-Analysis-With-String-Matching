d0e7163d9a290c1a4ffca05f9017bbfb94a0031b
henryanandsr <13521004@std.stei.itb.ac.id>
2023-05-23 21:59:36 +0700

    fix : brute force

--- src/BruteForce.py
+++ src/BruteForce.py
@@ -1,45 +1,40 @@
-import NewsAPI as news
+import nltk
+from nltk.corpus import opinion_lexicon
 
-# kata untuk sentiment positif dan negatif
-positive_words = ['beda', 'tampang', 'begini', 'buyback', 'saham', 'sukses', 'mudah', 'menarik', 'penuh', 'baik', 'puas', 'mantap', 'kolaborasi', 'dividen', 'cuan', 'prospektif', 'menggiurkan', 'pameran', 'laba', 'meluncur', 'profit', 'cemerlang', 'nyaman', 'terkerek', 'berumur', 'bagus', 'bangga']
-negative_words = ['penipuan', 'rusak', 'mengecewakan', 'ribet', 'mahal', 'berisiko', 'seram', 'terbatas', 'buruk', 'sakit', 'menyebalkan', 'berseteru', 'rusuh', 'batal', 'tersandera', 'intervensi', 'pemalsuan', 'bangkrut', 'kerugian', 'bumerang', 'tak layak']
+nltk.download('opinion_lexicon')
 
-titles = news.getTitle()
+positive_words = set(opinion_lexicon.positive())
+negative_words = set(opinion_lexicon.negative())
+
 clean_titles = []
-for title in titles:
-    clean_title = title.rsplit("-", 1)[0].strip()
-    clean_titles.append(clean_title)
+with open("transformed_text.txt", "r") as file:
+    lines = file.readlines()
+    clean_titles.extend(lines)
+
 def driverBF(titles):
-    for title in titles:
-        title = title.lower()
-        title = title.replace(",", "").replace(".", "")
-        words = title.split()
+    with open("BruteForce.txt", "w") as output_file:
+        for title in titles:
+            title = title.replace(",", "").replace(".", "")
+            words = title.split()
 
-        sentiment_score = 0
-        for word in words:
-            print(word, end=' ')
-            for positive_word in positive_words:
-                if len(positive_word)<len(word):
-                    for i in range (len(word)):
-                        if (word[i:i+len(positive_word)] == positive_word):
-                            sentiment_score += 1
+            sentiment_score = 0
+            for word in words:
+                if word in positive_words:
+                    sentiment_score += 1
+                elif word in negative_words:
+                    sentiment_score -= 1
 
-            for negative_word in negative_words:
-                if len(negative_word)<len(word):
-                    for i in range (len(word)):
-                        if (word[i:i+len(negative_word)] == negative_word):
-                            sentiment_score -= 1
-        print()
-        if sentiment_score > 0:
-            sentiment = "Positive"
-        elif sentiment_score < 0:
-            sentiment = "Negative"
-        else:
-            sentiment = "Neutral"
+            if sentiment_score > 0:
+                sentiment = "Positive"
+            elif sentiment_score < 0:
+                sentiment = "Negative"
+            else:
+                sentiment = "Neutral"
 
-        print("Title:", title)
-        print("Sentiment:", sentiment)
-        print()
-
+            output_file.write("Title: " + title + "\n")
+            output_file.write("Sentiment: " + sentiment + "\n")
+            output_file.write("\n")
+            print("Title:", title)
+            print("Sentiment:", sentiment)
 
 driverBF(clean_titles)
